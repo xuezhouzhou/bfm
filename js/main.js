1cb798742d0a3463c944881552c45d6ff24eac83
xuezhouzhou <xzzphp@163.com>
2015-02-15 20:18:04 +0800

    优化修改

--- js/main.js
+++ js/main.js
@@ -592,11 +592,8 @@ var run = function(){
       type: '', // 分享类型,music、video或link，不填默认为link
       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
       success: function () { 
-          // 用户确认分享后执行的回调函数
+        // 用户确认分享后执行的回调函数
       },
-      cancel: function () { 
-          // 用户取消分享后执行的回调函数
-      }
     });
   }
 }
