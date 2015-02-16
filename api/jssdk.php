a65ce3322100fad37543844af13b53ccdf689350
xuezhouzhou <xzzphp@163.com>
2015-02-17 01:38:20 +0800

    微信接口测试

--- api/jssdk.php
+++ api/jssdk.php
@@ -45,7 +45,7 @@ class JSSDK {
 
   private function getJsApiTicket() {
     // jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
-    @$data = json_decode(file_get_contents("jsapi_ticket.json"));
+    $data = json_decode(file_get_contents("jsapi_ticket.json"));
     if ($data->expire_time < time()) {
       $accessToken = $this->getAccessToken();
       // 如果是企业号用以下 URL 获取 ticket
@@ -56,9 +56,9 @@ class JSSDK {
       if ($ticket) {
         $data->expire_time = time() + 7000;
         $data->jsapi_ticket = $ticket;
-        @$fp = fopen("jsapi_ticket.json", "w");
-        @fwrite($fp, json_encode($data));
-        @fclose($fp);
+        $fp = fopen("jsapi_ticket.json", "w");
+        fwrite($fp, json_encode($data));
+        fclose($fp);
       }
     } else {
       $ticket = $data->jsapi_ticket;
@@ -79,9 +79,9 @@ class JSSDK {
       if ($access_token) {
         $data->expire_time = time() + 7000;
         $data->access_token = $access_token;
-        @$fp = fopen("access_token.json", "w");
-        @fwrite($fp, json_encode($data));
-        @fclose($fp);
+        $fp = fopen("access_token.json", "w");
+        fwrite($fp, json_encode($data));
+        fclose($fp);
       }
     } else {
       $access_token = $data->access_token;
--- cdn/data.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "jssdk.php";
-
-$jssdk = new JSSDK("wxd31771b7d224b883", "60fd9ce92eb04c67514d9a971484e2d1");
-
-$signPackage = $jssdk->GetSignPackage();
-
-echo json_encode($signPackage);
