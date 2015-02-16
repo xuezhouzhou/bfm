d19ed62622103746d4cb12d023a66b441caf5bff
xuezhouzhou <xzzphp@163.com>
2015-02-16 23:29:59 +0800

    微信分享接口测试

--- wxshare.php
+++ wxshare.php
@@ -1,6 +1,6 @@
 <?php
 $cdnUrl = "";
-require_once "api/jssdk.php";
+require_once "jssdk.php";
 $jssdk = new JSSDK("wxd31771b7d224b883", "60fd9ce92eb04c67514d9a971484e2d1");
 $signPackage = $jssdk->GetSignPackage();
 ?>
