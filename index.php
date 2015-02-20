54c22833f2b0be3c085f690b5113f3577264cc8d
karry <karrykai@gmail.com>
2015-02-20 14:13:10 +0800

    CDN改为1.0.7

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-header('Location:http://www.yourdream.cc/index.php');
-exit();
+//header('Location:http://www.yourdream.cc/index.php');
+//exit();
 $cdnUrl = "http://download.yourdream.cc/bfm/1.0.7/";
 require_once "jssdk.php";
 $jssdk = new JSSDK("wxd31771b7d224b883", "60fd9ce92eb04c67514d9a971484e2d1");
