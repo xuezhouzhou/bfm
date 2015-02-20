18ef461f7fdc9c51cbed4d9c29dc37f1e3295ee9
karry <karrykai@gmail.com>
2015-02-20 15:01:27 +0800

    [Modify Feature]开调试测试

--- cdn/index-debug.php
+++ cdn/index-debug.php
@@ -1,6 +1,6 @@
 <?php
 $cdnUrl = "http://download.yourdream.cc/bfm/1.0.7/";
-require_once "../jssdk.php";
+require_once "jssdk.php";
 $jssdk = new JSSDK("wxd31771b7d224b883", "60fd9ce92eb04c67514d9a971484e2d1");
 $signPackage = $jssdk->GetSignPackage();
 ?>
