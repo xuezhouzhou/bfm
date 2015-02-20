593120019487a50e853da47d06fbd2ed915a5fea
karry <karrykai@gmail.com>
2015-02-20 13:56:20 +0800

    调整CDN路径测试

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //header('Location:http://www.yourdream.cc/index.php');
 //exit();
-$cdnUrl = "http://android.yourdream.cc/bfm/cdn/";
+$cdnUrl = "http://android.yourdream.cc/bfm/";
 require_once "jssdk.php";
 $jssdk = new JSSDK("wxd31771b7d224b883", "60fd9ce92eb04c67514d9a971484e2d1");
 $signPackage = $jssdk->GetSignPackage();
