274494c7846a351c4c7787d880fcc0062671ef6c
karry <karrykai@gmail.com>
2015-02-20 13:51:04 +0800

    调整CDN路径测试

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header('Location:http://www.yourdream.cc/index.php');
 exit();
-$cdnUrl = "http://download.yourdream.cc/bfm/1.0.6/";
+$cdnUrl = "http://android.yourdream.cc/bfm/cdn/";
 require_once "jssdk.php";
 $jssdk = new JSSDK("wxd31771b7d224b883", "60fd9ce92eb04c67514d9a971484e2d1");
 $signPackage = $jssdk->GetSignPackage();
